b33f063841c68dc672d976d38957b21c759ef13d
coffeemori92 <coffeemori92@naver.com>
2019-06-12 17:01:59 +0900

    11

--- UserManagement/app/src/main/java/com/example/usermanagement/LoginActivity.java
+++ UserManagement/app/src/main/java/com/example/usermanagement/LoginActivity.java
@@ -2,7 +2,13 @@ package com.example.usermanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText idText = (EditText)findViewById(R.id.id_text);
+        EditText passwordText = (EditText)findViewById(R.id.password_text);
+
+        Button loginButton = (Button)findViewById(R.id.login_button);
+        TextView registerButton = (TextView)findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- UserManagement/app/src/main/java/com/example/usermanagement/RegisterActivity.java
+++ UserManagement/app/src/main/java/com/example/usermanagement/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.usermanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        EditText idText = (EditText)findViewById(R.id.id_text);
+        EditText passwordText = (EditText)findViewById(R.id.password_text);
+        EditText nameText = (EditText)findViewById(R.id.name_text);
+        EditText ageText = (EditText)findViewById(R.id.age_text);
+
+        Button registerButton = (Button)findViewById(R.id.register_button);
     }
 }
